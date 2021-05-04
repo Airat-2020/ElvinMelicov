0f5a9c81c0a15eb70c90d8b28cd1210533c98123
Agalar Ashurbekov <a.ashurbekov@cm.expert>
2021-05-04 16:12:57 +0300

    feat(all): setup git hooks  #ID-28

--- src/blocks/modules/nav/nav.js
+++ src/blocks/modules/nav/nav.js
@@ -2,10 +2,10 @@ let mainNav = document.querySelector(".main-nav");
 let buttonOpen = document.querySelector(".header__open-menu");
 let buttonClose = document.querySelector(".header__close-menu");
 
-buttonOpen.addEventListener("click", function (evt) {
+buttonOpen.addEventListener("click", function () {
     mainNav.classList.add("show");
 })
 
-buttonClose.addEventListener("click", function (evt) {
+buttonClose.addEventListener("click", function () {
     mainNav.classList.remove("show");
 })
