cd3b29645f3be07c959e73621a5e25414605fa29
Your Name <65311134+Airat-2020@users.noreply.github.com>
2021-05-04 09:54:22 +0300

    feat(src) add works module ID-17

---
+++ src/blocks/modules/works/works.js
@@ -0,0 +1,5 @@
+// import Swiper from 'swiper/bundle';
+// console.log('test')
+// const swiper = new Swiper('.swiper-container');
+//
+// console.log(swiper)
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,5 +1,4 @@
 import "%modules%/header/header";
 import "%modules%/nav/nav";
-import "%modules%/promo/promo";
-import "%modules%/pathology/pathology";
+import "%modules%/works/works";
 import "%modules%/footer/footer";
