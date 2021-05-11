a0162f1589e52a51f8791ea63ca284fe4bb2b30a
Your Name <65311134+Airat-2020@users.noreply.github.com>
2021-05-11 18:06:18 +0300

    feat(src): add block "consultations" ID-22

---
+++ src/blocks/modules/consultations/consultations.js
@@ -0,0 +1,13 @@
+modules.define('consultations', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
