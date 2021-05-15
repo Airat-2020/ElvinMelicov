6a802b7eca73d43d5759741aeb5803d4f975d224
Your Name <65311134+Airat-2020@users.noreply.github.com>
2021-05-15 18:32:30 +0300

    feat(src): add desktop version and slider of block "my works" ID-17

--- src/blocks/modules/works/works.js
+++ src/blocks/modules/works/works.js
@@ -1,7 +1,46 @@
-// import Swiper from 'swiper/bundle';
-// console.log('test')
-// const swiper = new Swiper('.swiper-container');
-//
-// console.log(swiper)
+import Swiper from 'swiper/bundle';
+new Swiper(".swiper-container",{
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-// new Swiper(".swiper-container");
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // loop: true,
+
+    initialSlide: 2,
+
+    grabCursor: true,
+
+    slideToClickedSlide: true,
+
+    hashNavigation: {
+        watchState: true,
+    },
+
+    keyboard: {
+        enabled: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+        eventsTarget: ".works__slider",
+    },
+
+    centeredSlides: true,
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+
+        1200: {
+            slidesPerView: 3,
+        },
+    },
+});
