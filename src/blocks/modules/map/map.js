a26685d6f8ee782d9c673dfc70fb2e582ea5e49e
Your Name <65311134+Airat-2020@users.noreply.github.com>
2021-06-03 19:19:51 +0300

    fix(map): add map from API ID-25

--- src/blocks/modules/map/map.js
+++ src/blocks/modules/map/map.js
@@ -1,1 +1,28 @@
+/* eslint-disable */
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.796128, 49.106416],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
 
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/svg/marker.svg',
+            // Размеры метки.
+            iconImageSize: [62, 70],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
